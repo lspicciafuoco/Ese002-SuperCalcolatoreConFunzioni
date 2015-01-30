ccb1e641df678bc3cb671ab5b8289cf7b5210da5
Alessio Picciafuoco <alessio.picciafuoco@gmail.com>
2015-01-30 12:08:03 +0100

    a

---
+++ mialibreria.h
@@ -0,0 +1,22 @@
+/* 
+ * File:   mialibreria.h
+ * Author: Alessio
+ *
+ * Created on 30 gennaio 2015, 9.44
+ */
+
+
+
+float somma(float a, float b);
+
+float sottrazione(float a, float b);
+
+float prodotto(float a, float b);
+
+float divisione(float a, float b);
+
+int quoziente(int a, int b);
+
+float frazionale(int a, int b);
+
+float potenza(float base, int esponente);
